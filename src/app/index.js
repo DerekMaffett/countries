2cb24022d5726e642fb6607e84427e1872d49949
Derek Maffett <derekgmaffett@gmail.com>
2017-11-16 04:37:56 +0100

    clean up

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import styles from './styles.css';
-console.log(styles);
 
 class App extends React.Component {
     render() {
         return (
-            <div className={styles.stuff}>
-                stuf
+            <div className='stuff'>
+                stuff
             </div>
         );
     }
