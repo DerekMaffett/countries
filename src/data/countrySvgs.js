4fd9d5ce5321ddced7a6770e42f2eab5f5736c14
Derek Maffett <derekgmaffett@gmail.com>
2017-11-16 07:44:17 +0100

    sort countries more sensibly

--- src/data/countrySvgs.js
+++ src/data/countrySvgs.js
@@ -1,6 +1,15 @@
+import { map, sortBy, flow } from 'lodash';
+
 const countryCodeMap = require('../../node_modules/svg-country-flags/countries.json');
 
-export default Object.keys(countryCodeMap).map(code => ({
+const convertCodeMapToArray = codeMap => map(codeMap, (name, code) => ({
     id: code,
     name: countryCodeMap[code]
 }));
+
+const sortByName = countries => sortBy(countries, 'name');
+
+export default flow([
+    convertCodeMapToArray,
+    sortByName
+])(countryCodeMap);
