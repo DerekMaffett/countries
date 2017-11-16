e8ed21ecb646a70e54c3ac610b412670c36bc422
Derek Maffett <derekgmaffett@gmail.com>
2017-11-16 01:40:53 +0100

    enable react and es6 loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-	entry: './src',
+	entry: './src/index.jsx',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js'
-	}
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: '.'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['babel-preset-env', 'react']
+					}
+				}
+			}
+		]
+	},
+	plugins: []
 };
 
 module.exports = config;
